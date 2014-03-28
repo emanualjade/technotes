a5723cf667ba4bb6555ccf4d2ea692b7564048e6
Emanual Jade <emanualjade@gmail.com>
2014-03-27 21:45:06 -0700

    Add rails association notes

--- Rails/many_to_many_association.rb
+++ Rails/many_to_many_association.rb
@@ -1,6 +1,9 @@
 AdminUser has_and_belongs_to_many :pages
 Page has_and_belongs_to_many :admin_users
 
+Course has_and_belongs_to_many :students
+Student has_and_belongs_to_many :courses
+
 # RAILS CONVENTION FOR CREATING A JOIN TABLE
 # first_table + _ + second_table
 # Both table names are plural
---
+++ Rails/one_to_one_association.rb
@@ -0,0 +1,6 @@
+# One-to-one
+Classroom has_one :teacher
+Teacher belongs_to :classroom
+
+# Class with "belongs_to" should have the foreign key
+# Always define both sides of the relationship
