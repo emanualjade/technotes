8b43557d635e79398913c7ccf444039bdd43ad8d
Emanual Jade <emanualjade@gmail.com>
2014-12-16 23:49:14 -0800

    Add a basic class in a closure example

--- javascript/object-oriented/basic_class.js
+++ javascript/object-oriented/basic_class.js
@@ -1,6 +1,22 @@
-// BASIC CLASS //
+// BASIC CLASS VERSION ONE //
 function MyClass() {
   this.a = 42;
 }
 MyClass.prototype.method = function method() {};
 instance = new MyClass();
+
+// BASIC CLASS VERSION TWO //
+var SomeClass;
+
+SomeClass = (function() {
+  function SomeClass(name) {
+    this.name = name;
+  }
+
+  SomeClass.prototype.amethod = function(a) {
+    return console.log(this + a);
+  };
+
+  return SomeClass;
+
+})();
