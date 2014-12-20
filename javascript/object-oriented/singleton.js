bd8a89c2824fd99714efaa05122aedb472889937
Emanual Jade <emanualjade@gmail.com>
2014-12-20 10:33:06 -0800

    Add javascript singleton example

---
+++ javascript/object-oriented/singleton.js
@@ -0,0 +1,33 @@
+var dom = (function(){
+  var _counter = 0;
+  var instance;
+
+  function generateId(){
+    return "customId" + _counter++;
+  }
+
+  function create(tagName, id){
+    var el = document.createElement(tagName);
+    el.id = id || generateId();
+    return el;
+  }
+
+  function createInstance(){
+    return {
+      generateId: generateId,
+      create: create
+    };
+  }
+
+  return {
+    getInstance: function(){
+      return instance || (instance = createInstance());
+    }
+  }
+
+}());
+
+var el = dom.getInstance().create("div");
+console.log(el.id);
+var instance = dom.getInstance();
+console.log(instance === dom.getInstance()); //true
